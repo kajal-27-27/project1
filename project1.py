79386f924746555d33ccc260417b1b2221c6244c
kajal-27-27 <kajalrchaudhari2722007@gmail.com>
2024-12-09 08:37:56 +0530

    add

---
+++ project1.py
@@ -0,0 +1,11 @@
+x=int(input("enter your marks:"))
+if x >= 90:
+    print('Grade A')
+elif x >= 80 and x <= 89:
+    print('Grade B')
+elif x >= 70 and x <= 79:
+    print('Grade C')
+elif x >= 60 and x <= 69:
+    print('Grade D')
+else:
+    print('Grade F')
